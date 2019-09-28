f9e04138fca9041178b116655cbd6ca39439e3b9
bbi <bbaarroo@slack.com>
2019-09-28 14:28:10 +0200

    init

---
+++ src/main/java/otodom/otodom.java
@@ -0,0 +1,78 @@
+package otodom;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class otodom {
+
+    public static void main(String[] args) throws Exception {
+
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+
+        long start = System.currentTimeMillis();
+
+//        URL otodom = new URL("https://www.otodom.pl/sprzedaz/mieszkanie/poznan/piatkowo/");
+//        BufferedReader in = new BufferedReader(new InputStreamReader(otodom.openStream()));
+//
+//        String inputLine;
+//        StringBuilder stringBuilder = new StringBuilder();
+//        while ((inputLine = in.readLine()) != null) {
+//            stringBuilder.append(inputLine);
+//            stringBuilder.append(System.lineSeparator());
+//        }
+//        in.close();
+//        System.out.println(stringBuilder.toString());
+
+
+        final String OTODOM_URL = "https://www.otodom.pl/sprzedaz/mieszkanie/poznan/piatkowo/";
+
+        Document doc = Jsoup.connect(OTODOM_URL).get();
+        Elements allLinks = doc.select("a[href]");
+
+        Set<String> links = new HashSet<>();
+        for (Element element : allLinks) {
+            String href = element.attr("href");
+            if (href.contains("https://www.otodom.pl/oferta/"))
+                links.add(href);
+        }
+
+
+        for (int i = 0; i < links.size(); i++) {
+            int finalI = i;
+            executorService.submit(()-> {
+                try {
+                    savePages(links.toArray()[finalI].toString(), finalI + ".html");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+
+        executorService.shutdown();
+
+        links.forEach(System.out::println);
+        long end = System.currentTimeMillis();
+        System.out.println("ilość pobranych linkow" + links.size());
+        System.out.println("Czas wykonania: " + (end - start));
+
+    }
+
+    public static void savePages(String link, String filename) throws IOException {
+        Document doc = Jsoup.connect(link).get();
+        FileWriter fileWriter = new FileWriter(filename, false);
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+        bufferedWriter.write(doc.toString());
+        bufferedWriter.close();
+    }
+
+}
